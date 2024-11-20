3059526c0737ebe3c3d4844dd95dddd39d7f5988
MoloTheSniper <85521815+MoloTheSniper@users.noreply.github.com>
2024-11-20 17:47:52 +0200

    Moved Index out of (TABS) folder and placed it into APPS folder

--- app/index.tsx
+++ app/index.tsx
@@ -1,23 +1,24 @@
 import { StyleSheet, Text, View } from 'react-native'
-import { StatusBar } from 'expo-status-bar'
+import { StatusBar } from 'expo-status-bar';
+import {Link} from 'expo-router';
 import React from 'react'
-import {Link} from 'expo-router'
 
 export default function App()
 {
-  return(
-    <View style = {styles.container}>
-      <Text>Syrup</Text>
-      <StatusBar style = "auto"/>
-      <Link href ="/profile" style={{color:'blue'}}>Go to Profile</Link>
-    </View>
-  )
+    return (
+        <View style = {styles.container}>
+            <Text>Syrup!</Text>
+            <StatusBar style='auto'/>
+            <Link href = "/profile" style ={{color: 'blue'}}>Go To Profile</Link>
+        </View>
+    );
 }
+
 const styles = StyleSheet.create({
-  container:{
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent:'center',
-  }
+    container:{
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    }
 })
