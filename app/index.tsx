5fe5433c0da26a3022262ac19ad440c4ba4e51b6
MoloTheSniper <85521815+MoloTheSniper@users.noreply.github.com>
2024-11-20 12:58:21 +0200

    Moved index page from (tabs) to app

---
+++ app/index.tsx
@@ -0,0 +1,24 @@
+import { StyleSheet, Text, View } from 'react-native'
+import { StatusBar } from 'expo-status-bar';
+import {Link} from 'expo-router';
+import React from 'react'
+
+export default function App()
+{
+    return (
+        <View style = {styles.container}>
+            <Text>Syrup!</Text>
+            <StatusBar style='auto'/>
+            <Link href = "/profile" style ={{color: 'blue'}}>Go To Profile</Link>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container:{
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    }
+})
